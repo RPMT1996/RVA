92b5f1195c922ea2bf4204022f83e22c297aad42
Teresa Peixoto <8190334@estg.ipp.pt>
2023-12-07 16:04:17 +0000

    update boards teleporte and main camera

---
+++ Assets/Scripts/ToolsAndTrees/HammerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerController : MonoBehaviour
+{
+    public Collider hammerTipCollider; 
+
+    private Rigidbody hammerRb;
+    private float breakThreshold = 8f;
+
+
+    private void Start()
+    {
+        hammerRb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //check if the collision is with the axe tip's collider
+        if (collision.contacts[0].thisCollider == hammerTipCollider && collision.gameObject.CompareTag("teste"))
+        {
+            
+                collision.gameObject.transform.GetComponent<transformController>().BreakHood();
+            
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
